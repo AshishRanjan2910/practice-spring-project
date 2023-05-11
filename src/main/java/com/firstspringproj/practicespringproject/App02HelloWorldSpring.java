a256e3ce5ef59727d0b33318f1fde554b2f19c72
Ashish Ranjan <67757899+AshishRanjan2910@users.noreply.github.com>
2023-05-12 01:47:05 +0530

    more ways of making beans explored

--- src/main/java/com/firstspringproj/practicespringproject/App02HelloWorldSpring.java
+++ src/main/java/com/firstspringproj/practicespringproject/App02HelloWorldSpring.java
@@ -5,6 +5,9 @@ public class App02HelloWorldSpring {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("person2MethodCall"));
     }
 
 }
--- src/main/java/com/firstspringproj/practicespringproject/HelloWorldConfiguration.java
+++ src/main/java/com/firstspringproj/practicespringproject/HelloWorldConfiguration.java
@@ -3,10 +3,33 @@ package com.firstspringproj.practicespringproject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {
+};
+
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name() {
-        return "returning name bean...";
+        return "Ash";
+    }
+
+    @Bean
+    public int age() {
+        return 23;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Kush", 44);
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age){
+        return new Person(name, age);
     }
 }
